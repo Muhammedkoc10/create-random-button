d5b1ea99fbc7bd241a1e67838e985d193abad42a
Muhammed <muhammedkoc10@gmail.com>
2022-06-21 23:08:23 +0300

    create-rnd-button

---
+++ Create-rnd-button/Koleksiyonlar/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Koleksiyonlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpButon = new System.Windows.Forms.FlowLayoutPanel();
+            this.rbTek = new System.Windows.Forms.RadioButton();
+            this.rbCift = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // flpButon
+            // 
+            this.flpButon.Location = new System.Drawing.Point(84, 120);
+            this.flpButon.Name = "flpButon";
+            this.flpButon.Size = new System.Drawing.Size(400, 253);
+            this.flpButon.TabIndex = 0;
+            // 
+            // rbTek
+            // 
+            this.rbTek.AutoSize = true;
+            this.rbTek.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbTek.Location = new System.Drawing.Point(174, 80);
+            this.rbTek.Name = "rbTek";
+            this.rbTek.Size = new System.Drawing.Size(50, 21);
+            this.rbTek.TabIndex = 1;
+            this.rbTek.TabStop = true;
+            this.rbTek.Text = "Tek";
+            this.rbTek.UseVisualStyleBackColor = true;
+            this.rbTek.CheckedChanged += new System.EventHandler(this.rbTek_CheckedChanged);
+            // 
+            // rbCift
+            // 
+            this.rbCift.AutoSize = true;
+            this.rbCift.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbCift.Location = new System.Drawing.Point(310, 80);
+            this.rbCift.Name = "rbCift";
+            this.rbCift.Size = new System.Drawing.Size(46, 21);
+            this.rbCift.TabIndex = 1;
+            this.rbCift.TabStop = true;
+            this.rbCift.Text = "Çift";
+            this.rbCift.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(310, 80);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 21);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Çift";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 400);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rbCift);
+            this.Controls.Add(this.rbTek);
+            this.Controls.Add(this.flpButon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpButon;
+        private System.Windows.Forms.RadioButton rbTek;
+        private System.Windows.Forms.RadioButton rbCift;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Create-rnd-button/Koleksiyonlar/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Koleksiyonlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList sayilar = new ArrayList();
+        
+        Random rnd = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                int uretilenSayi = rnd.Next(20, 80);
+                if (sayilar.IndexOf(uretilenSayi) == -1)
+                {
+                    Button buton = new Button();
+                    buton.Width = 30;
+                    buton.Height = 30;
+                    buton.Text = uretilenSayi.ToString();
+                    flpButon.Controls.Add(buton);
+                    sayilar.Add(uretilenSayi);
+                }
+                else
+                    i--;
+            }
+        }
+
+        private void rbTek_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Button item in flpButon.Controls)
+            {
+                int butonSayi = Convert.ToInt32((item as Button).Text);
+
+                if (butonSayi %2 != 0)
+                    (item as Button).Enabled = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Button item in flpButon.Controls)
+            {
+                int butonSayi = Convert.ToInt32((item as Button).Text);
+
+                if (butonSayi % 2 == 0)
+                    (item as Button).Enabled = false;
+
+            }
+        }
+    }
+}
